5cba6be59d67fd064f1e5040838ed8ffc24444f2
Kasun <kasungayashan789@gmail.com>
2024-05-05 11:59:15 +0530

    #2 - completed Splash page

---
+++ lib/pages/languageScreen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class LangageScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Langage"),
+      ),
+      body: Center(
+        child: Text("Langage"),
+      ),
+    );
+  }
+  
+}
--- lib/pages/splashPage.dart
+++ lib/pages/splashPage.dart
@@ -1,42 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
+import 'package:menupro/pages/languageScreen.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+  @override
+  State<SplashPage> createState() => _SplashScreenState();
+}
+
+ class _SplashScreenState extends State<SplashPage>
+ with SingleTickerProviderStateMixin{
+  @override
+  void initState(){
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+    Future.delayed(const Duration(seconds: 5), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (_) => LangageScreen()));
+    });
+  }
+
+  @override
+  void dispose(){
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+    overlays: SystemUiOverlay.values
+    );
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: SingleChildScrollView(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Image.asset(
-                '../../assets/menuproiconlighttheam.png',
-                height: 200,
-                width: 200,
+              Padding(
+                padding: EdgeInsets.only(left: 20.0),
+                child: Image.asset(
+                  'assets/images/menuproiconlighttheam.png',
+                  height: 200,
+                  width: 200,
+                ),
               ),
               SizedBox(
                 height: 50,
               ),
               Image.asset(
-                '../../assets/connies.png',
+                'assets/images/connies.png',
                 height: 200,
                 width: 200,
               ),
               SizedBox(
-                height: 90,
+                height: 80,
               ),
-              Text("Crafted menu \n experience.",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-                color: Color.fromRGBO(27, 27, 27, 1.0)
-              ) ,),
+              Text(
+                "Crafted menu \n experience.",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'GothomProBlack',
+                    fontSize: 20,
+                    color: Color.fromRGBO(27, 27, 27, 1.0)),
+              ),
+              Image.asset(
+                'assets/images/line.png',
+                height: 300,
+                width: 600,
+              ),
             ],
           ),
         ),
       ),
-    );
+    ); 
   }
-}
+  }
